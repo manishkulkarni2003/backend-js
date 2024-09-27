292da771308bf25fff1ff590d954575e6182a770
manishkulkarni2003 <mk3304939@gmail.com>
2024-09-27 19:09:02 +0530

    custom api response and error handling

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors"
+import cookieParser from "cookie-parser";
+
+const app = express()
+
+//app.use is used for middlewares and configurations 
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+
+}))
+
+app.use(express.json({ limit: "16kb" }))
+//url encode hone ke liye 
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))
+
+//file ko store karta hai
+app.use(express.static("public"))
+
+app.use(cookieParser())
+
+
+
+export { app }
+
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,43 @@
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+
+
+
+
+export { asyncHandler }
+
+
+
+
+
+
+
+
+
+
+
+//wrapper function
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
+
+
+
+
+// const asyncHandler=()=>{
+
+// }
+// const asyncHandler=(func)=>async()=>{}
+//we are passing a function to the other function as param
